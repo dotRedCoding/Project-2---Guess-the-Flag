f1cd1022782a553ed9c964d416dbdd7d37c61092
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-04-23 12:06:57 -0400

    Initial Commit

---
+++ Project 2 - Guess the Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2 - Guess the Flag
+//
+//  Created by Jared Infantino on 2023-04-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
