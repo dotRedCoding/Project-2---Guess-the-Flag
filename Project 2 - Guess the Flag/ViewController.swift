d3a6757ea6ebbac752724ad2cc8d0b4f5177e050
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-04-27 10:17:04 -0400

    - button tap now implemented - tag values assigned to the buttons, during the game if the correct button tag matches the random int assigned at the top score will go up 1 or down 1 - basic functionality is finished and application is working

--- Project 2 - Guess the Flag/ViewController.swift
+++ Project 2 - Guess the Flag/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,10 +52,38 @@ class ViewController: UIViewController {
 
     // MARK: - Question
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle() // will shuffle the array around so the order will change
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    // MARK: - Button Tap
+    
+    // IBOutlet = connecting code to storyboard layout
+    // IBAction = making storyboard layouts trigger code
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Incorrect!"
+            score -= 1
+        }
+        
+        // closure - pop up alert
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) // add button to the alert, no parenthesis on the ask question
+        
+        present(ac, animated: true)
     }
     
 }
